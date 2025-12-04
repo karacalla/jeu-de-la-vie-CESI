5e3a80bea16418f924151e7d0ada692e1ac5ad3c
Spectre9813 <arthurchant2006@gmail.com>
2025-12-04 22:03:52 +0100

    Implement Console class with display and clear methods

---
+++ Console.cpp
@@ -0,0 +1,31 @@
+#include "Console.h"
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+
+Console::Console() {}
+
+Console::~Console() {}
+
+void Console::displayConsole(Matrice mat) {
+    for (int i = 0; i < mat.getRow(); i++) {
+        for (int j = 0; j < mat.getColumn(); j++) {
+            if (mat.matrice[i][j] == 0) {
+                cout << "  ";
+            } else {
+                cout << "██";
+            }
+        } cout << endl;
+    }
+}
+
+void Console::clearConsole() {
+    if (WIN32) {
+        system("cls");
+    } else {
+        system("clear");
+    }
+}
