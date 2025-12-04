3b8df58d08b76dd3dab05547440eef07329b12ef
karacalla <caracallam@gmail.com>
2025-12-04 09:56:17 +0100

    created skeleton

---
+++ parse.h
@@ -0,0 +1,23 @@
+#ifndef POO_PARSE_H
+#define POO_PARSE_H
+
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <ctime>
+
+
+class parse {
+   
+    private:
+
+    // stuff here
+
+
+    public:
+
+    // even more stuff (wow really?)
+
+}
