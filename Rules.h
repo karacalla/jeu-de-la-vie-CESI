e4b5b6d4114218562f4b59403569c1a998bc06a5
Spectre9813 <arthurchant2006@gmail.com>
2025-12-05 14:57:10 +0100

    Refactor Rules.h header guards and remove unused variables

--- Rules.h
+++ Rules.h
@@ -1,23 +1,10 @@
-#ifndef TEST_RULES_H
-#define TEST_RULES_H
+#ifndef JEU_DE_LA_VIE_RULES_H
+#define JEU_DE_LA_VIE_RULES_H
 
-// Règle pour le nombre de cellule maximum et minimum
+// Règle pour le nombre de cellule maximum et minimum (B3/S23)
 inline int NB_MAX_CELL = 3;
 inline int NB_MIN_CELL = 2;
 
-// Chemin d'accès vers le fichier de sauvegarde et le fichier initial
-inline char INIT_FILE_PATH = *"Matrice.txt";
-inline char SAVE_FILE_PATH = *"Save.txt";
+inline int NB_BORN_CELL = 3;
 
-// Nombre d'itérations nécessaires pour faire une sauvegarde
-inline int NB_ITERATIONS = 10;
-
-// Compteur d'itérations
-inline int cptIterations = 0;
-
-// Gestion de l'affichage
-inline bool UI = false;
-inline bool CONSOLE = true;
-
-
-#endif //TEST_RULES_H
+#endif //JEU_DE_LA_VIE_RULES_H
