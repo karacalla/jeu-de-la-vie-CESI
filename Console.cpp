ebace39f73c289a48e5ca0b49cc2993c006705cc
Spectre9813 <arthurchant2006@gmail.com>
2025-12-05 15:04:16 +0100

    Refactor Console class and update display method

--- Console.cpp
+++ Console.cpp
@@ -1,21 +1,23 @@
 #include "Console.h"
-#include <vector>
-#include <string>
+#include "Matrice.h"
+#include "Cell.h"
+#include "Rules.h"
+#include "Config.h"
 #include <iostream>
 #include <fstream>
-#include <cstdlib>
+#include <string>
 using namespace std;
 
-Console::Console() {}
+Console::Console() = default;
 
-Console::~Console() {}
+Console::~Console() = default;
 
-void Console::displayConsole(Matrice mat) {
+void Console::drawConsole(Matrice mat) {
     for (int i = 0; i < mat.getRow(); i++) {
         for (int j = 0; j < mat.getColumn(); j++) {
-            if (mat.matrice[i][j] == 0) {
+            if (mat.matrice[i][j].getState() == 0 || mat.matrice[i][j].getState() == 2) {
                 cout << "  ";
-            } else {
+            } else if (mat.matrice[i][j].getState() == 1 || mat.matrice[i][j].getState() == 3) {
                 cout << "██";
             }
         } cout << endl;
